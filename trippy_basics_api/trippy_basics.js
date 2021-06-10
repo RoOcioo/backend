a490a3307d207afba43ca9bd32dfc49d9f92b3f1
Rocio <arias.roocio@gmail.com>
2021-06-10 17:27:24 +0200

    routes hotel

--- trippy_basics_api/trippy_basics.js
+++ trippy_basics_api/trippy_basics.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect('mongodb://localhost:27017/trippy_basics', (err) => {
+mongoose.connect('mongodb://localhost:27017/trippy_basics',
+{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) {
     console.log(err);
   } else {
@@ -23,6 +24,33 @@ mongoose.connect('mongodb://localhost:27017/trippy_basics', (err) => {
 app.get('/', (req, res) => {
   res.json('Trippy');
 });
+
+
+app.get('/hotels', (req, res) => {
+  res.json('hotelsroute');
+});
+
+
+app.get('/hotels/:id', (req, res) => {
+  res.json('hotelsidroute');
+});
+
+
+app.post('/hotels', (req, res) => {
+  console.log(req.body);
+});
+
+
+app.put('/hotels/:id', (req, res) => {
+  console.log(req.query);
+  res.json('nouveauhotel');
+});
+
+
+app.delete('/hotels/:id', (req, res) => {
+  res.json('effacerhotel');
+});
+
 
 app.listen(port, () => {
   console.log(`Serveur à l'écoute dans le port ${port} `);
