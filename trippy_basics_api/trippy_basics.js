6548050d58ce236acfd7087cc838e4f5197694f4
Rocio <arias.roocio@gmail.com>
2021-06-12 16:27:15 +0200

    crud hotel finish

--- trippy_basics_api/trippy_basics.js
+++ trippy_basics_api/trippy_basics.js
@@ -4,52 +4,106 @@ const mongoose = require('mongoose');
 const Restaurant = require('./model/restaurant');
 const Hotel = require('./model/hotel');
 
+
+mongoose.connect('mongodb://localhost:27017/trippy_basics',
+  { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected");
+    }
+  });
+
 const port = 8000;
 
 const app = express();
 
+const debug = (req, res, next) => {
+  console.log("I received a request!");
+
+  next()
+}
+
 app.use(cors());
 app.use(express.json());
-
-
-mongoose.connect('mongodb://localhost:27017/trippy_basics',
-{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Connected");
-  }
-});
-
-app.get('/', (req, res) => {
-  res.json('Trippy');
-});
+app.use(debug)
 
 
 app.get('/hotels', (req, res) => {
-  res.json('hotelsroute');
+  try {
+    const hotels = await Hotel.find()
+    res.json('hotelsroute');
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ errormessage: "There was a problem :(" })
+  }
 });
 
 
-app.get('/hotels/:id', (req, res) => {
-  res.json('hotelsidroute');
+app.get('/hotels/:id', async (req, res) => {
+  try {
+    const hotelId = req.params.id
+    const hotel = await Hotel.findById(hotelId)
+    if (hotel) {
+      res.json({ hotel })
+    } else {
+      res.json({
+        message: "Hotel not found"
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ errormessage: "There was a problem :(" })
+  }
 });
 
 
-app.post('/hotels', (req, res) => {
-  console.log(req.body);
+app.post('/hotels', async (req, res) => {
+  try {
+    const hotel = req.body
+    const newHotel = await Hotel.create(hotel)
+    res.json({
+      message: "New Hotel"
+    })
+  } catch (err) {
+    console.log(error)
+    res.status(500).json({ errormessage: "There was a problem :(" })
+  }
 });
 
 
-app.put('/hotels/:id', (req, res) => {
-  console.log(req.query);
-  res.json('nouveauhotel');
+app.put('/hotels/:id', async (req, res) => {
+  try {
+    const hotelId = req.query.id
+    const name = req.query.name
+    console.log('HotelId', hotelId, 'name', name);
+    const hotel = await Hotel.updateOne({ _id: hotelId }, { name: name })
+    res.json('nouveauhotel');
+  } catch (err) {
+    res.status(500).json({ errormessage: "There was a problem :(" })
+  }
 });
 
 
-app.delete('/hotels/:id', (req, res) => {
-  res.json('effacerhotel');
-});
+app.delete('/hotels/:id', async (req, res) => {
+  try {
+    const hotelId = req.params.id
+    const effacerHotel = await Hotel.findByIdAndDelete(hotelId)
+
+    res.json({
+      message: 'effacerhotel'
+    })
+  } catch (err) {
+    console.log(error);
+    res.status(500).json({ errormessage: "There was a problem :(" })
+  }
+})
+
+app.get("*", (req, res) => {
+  res.json({
+    errorMessage: "The route was not found"
+  }, 404)
+})
 
 
 app.listen(port, () => {
